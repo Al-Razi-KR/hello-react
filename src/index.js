3cca493579ed24225423485f931832d8f7fdd764
Al-Razi-KR <mohammedrazikr9419@gmail.com>
2021-01-11 20:52:58 +0800

    Apps

--- src/index.js
+++ src/index.js
@@ -2,8 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const studentList1 = [
+  {id:"1", name:"Abdu", place: "IND"},
+  {id:"2", name:"Basil", place: "US"},
+  {id:"3", name:"Darshan", place: "SIN"},
+  {id:"4", name:"Razi", place: "UK"}
+];
+
+const studentList2 = [
+  {id:"1", name:"Abu", place: "RUS"},
+  {id:"2", name:"Bilal", place: "AUS"},
+  {id:"3", name:"Don", place: "NIG"},
+  {id:"4", name:"Jill", place: "ARG"}
+];
+
+
+function Student({ students }){
+  return(
+    <div>
+      {students.map(student => (
+        <div key={student.id}>
+          <h2>{student.id}  {student.name}</h2>
+          <p>Lives in {student.place}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function App(props){
+  if (props.pick === "1"){
+    return <Student  students={studentList1} />;
+  }
+  else if (props.pick === "2"){
+    return <Student  students={studentList2} />;
+  }
+}
+
 ReactDOM.render(
-  React.createElement("h1", {style: {color:"Blue"}}, "Hello, World!"),
+    <App pick="1"/>,
   document.getElementById('root')
 );
 
