e97e866f9cc48bf6cc9c67dc73a42381a18c4375
Al-Razi-KR <mohammedrazikr9419@gmail.com>
2021-01-12 14:37:52 +0800

    Revert back

--- src/index.js
+++ src/index.js
@@ -2,45 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const studentList1 = [
-  {id:"1", name:"Abdu", place: "IND"},
-  {id:"2", name:"Basil", place: "US"},
-  {id:"3", name:"Darshan", place: "SIN"},
-  {id:"4", name:"Razi", place: "UK"}
-];
-
-const studentList2 = [
-  {id:"1", name:"Abu", place: "RUS"},
-  {id:"2", name:"Bilal", place: "AUS"},
-  {id:"3", name:"Don", place: "NIG"},
-  {id:"4", name:"Jill", place: "ARG"}
-];
-
-
-function Student({ students }){
+function App(params) {
   return(
     <div>
-      {students.map(student => (
-        <div key={student.id}>
-          <h2>{student.id}  {student.name}</h2>
-          <p>Lives in {student.place}</p>
-        </div>
-      ))}
+      Hello
     </div>
   );
 }
 
-function App(props){
-  if (props.pick === "1"){
-    return <Student  students={studentList1} />;
-  }
-  else if (props.pick === "2"){
-    return <Student  students={studentList2} />;
-  }
-}
-
 ReactDOM.render(
-    <App pick="1"/>,
+  <App/>,
   document.getElementById('root')
 );
-
